ba4e04e916ba06966288e26bd2c319a330ca9aa2
Dilshan <dilshanamarasinghe049@gmail.com>
2024-03-30 19:40:22 -0700

    Initial Commit

---
+++ Clothing_App_IOS/Clothing_App_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clothing_App_IOSApp.swift
+//  Clothing_App_IOS
+//
+//  Created by NIBM-LAB04-PC03 on 2024-03-30.
+//
+
+import SwiftUI
+
+@main
+struct Clothing_App_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
